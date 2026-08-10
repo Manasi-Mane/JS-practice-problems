5e0dc92bdc76d76964495f5f064d55a27f0d581c
Manasi-Mane <manasimane711@gmail.com>
2026-08-10 14:42:30 +0530

    Login simulation

--- asyncAwaitEx.js
+++ asyncAwaitEx.js
@@ -1,15 +1,58 @@
-const getData = () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Data received");
-        }, 2000);
-    });
-};
+// const getData = () => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("Data received");
+//         }, 2000);
+//     });
+// };
 
-const fetchData = async () => {
-    const result = await getData();
+// const fetchData = async () => {
+//     const result = await getData();
 
-    console.log(result);
-};
+//     console.log(result);
+// };
 
-fetchData();
+// fetchData();
+
+// const getData = () => {
+//     return new Promise((resolve, reject) => {
+//         reject("Unable to get data");
+//     });
+// };
+
+// const fetchData = async () => {
+
+//     try {
+//         const result = await getData();
+//         console.log(result);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// };
+
+// fetchData();
+
+//login simulation
+
+const loginUser = (username,password)=>{
+    return new Promise((resolve,reject)=>{
+        if(username==="admin" && password==="admin123"){
+            resolve("Login successful");
+        }else{
+            reject("Invalid username or password");
+        }
+    })
+}
+
+const login = async ()=>{
+    try{
+        const result = await loginUser("admin","admin123");
+        console.log(result);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+login()
